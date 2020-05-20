e2810bd586ac18952777dc764033b01658ebdf94
Finn <Mateusz>
2020-05-20 17:42:59 +0200

    Added LedView contents and initial Led fields.

--- ViewModel/LedViewModel.cs
+++ ViewModel/LedViewModel.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace PiHatWPF.ViewModel
 {
     class LedViewModel:BaseViewModel
     {
+        private Dictionary<Tuple<int, int>, Rectangle> ledMatrix = new Dictionary<Tuple<int, int>, Rectangle>();
+        private List<Rectangle> selectedLeds = new List<Rectangle>();
+        public Grid ViewLedMatrix { get; set; }
+
+        public LedViewModel()
+        {
+
+        }
+
+        private void LedMatrixInit()
+        {
+            BrushConverter bc = new BrushConverter();
+            int[] initialColors = { 65535, 65535, 65535, 65535, 65535, 65535, 65535, 65535,
+                                    65535, 65535, 65535, 65535, 65535, 65535, 65535, 65535,
+                                    65535, 65535, 65535, 65535, 65535, 65535, 65535, 65535,
+                                    65535, 65535, 65535, 65535, 65535, 65535, 65535, 65535,
+                                    65535, 65535, 65535, 65535, 65535, 65535, 65535, 65535,
+                                    65535, 65535, 65535, 65535, 65535, 65535, 65535, 65535,
+                                    65535, 65535, 65535, 65535, 65535, 65535, 65535, 65535,
+                                    65535, 65535, 65535, 65535, 65535, 65535, 65535, 65535};
+        }
     }
 }
